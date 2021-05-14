62ea9c79d2e3b37ed91e119e9641ff7dfaf4571a
Anahit Apresyan <aapresya@c1r12s2.42yerevan.am>
2021-05-14 13:52:21 +0400

    d and i done

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -19,11 +19,17 @@ int ft_numlength(int num)
 	int i;
 
 	i = 0;
+	// if (num < 0)
+	// 	i++;
+	if (num == 0)
+		i = 1;
 	while (num != 0)
 	{
 		num /= 10;
 		i++;
 	}
+	if (t_flags.period && t_flags.precision == 0)
+		i = 0;
 	return (i);
 }
 
@@ -63,18 +69,27 @@ int ft_numlength(int num)
 
 void	ft_check_minus(char **f)
 {
-	if (*(*f) == '-')
-	{
-		t_flags.minus = 1;
-		(*f)++;
+
+	while (*(*f) == '-' || *(*f) == '0'){
+		if (*(*f) == '-')
+			t_flags.minus = 1;
+		if (*(*f) == '0')
+			t_flags.zero = 1;
+		(*f)++;	
 	}
-	if (*(*f) == '0')
-	{
-		t_flags.zero = 1;
-		(*f)++;
-	}
+	// if (*(*f) == '-')
+	// {
+	// 	t_flags.minus = 1;
+	// 	(*f)++;
+	// }
+	// if (*(*f) == '0')
+	// {
+	// 	t_flags.zero = 1;
+	// 	(*f)++;
+	// }
 	if (t_flags.zero == 1 && t_flags.minus == 1)
 		t_flags.zero = 0;
+	
 }
 
 int	ft_atoi2(char **str)
@@ -117,25 +132,48 @@ void ft_printf_int()
 {
 	int spaces;
 	int zeroes;
+	int num;
 
+	zeroes =  t_flags.precision ? t_flags.precision - ft_numlength((int)t_flags.arg) : 0;
 	if (t_flags.precision > ft_numlength((int)t_flags.arg))
 		spaces = t_flags.minWidth - t_flags.precision;
 	else
 		spaces = t_flags.minWidth - ft_numlength((int)t_flags.arg);	
-	zeroes = t_flags.precision - ft_numlength((int)t_flags.arg);
+	
+	if(t_flags.zero && t_flags.precision == 0){
+		zeroes += spaces;
+		spaces = 0;
+	}
+
+	num = (int)t_flags.arg;
+	
+	if ((int)t_flags.arg < 0)
+	{
+		num *= -1;
+		spaces--;
+
+		if (spaces <= 0 && zeroes > 0 && t_flags.precision == 0)
+			zeroes--;
+	}
 	if (!t_flags.minus)
 	{
 		while (spaces-- > 0)
 			ft_putchar_fd(' ', 1);
+		if ((int)t_flags.arg < 0)
+			ft_putchar_fd('-', 1);
 		while (zeroes-- > 0)
 			ft_putchar_fd('0', 1);
-		ft_putstr_fd(ft_itoa((int)t_flags.arg), 1);
+		if (!(t_flags.period && t_flags.precision == 0))
+			ft_putstr_fd(ft_itoa(num), 1);
 	}
 	else
 	{
+		if ((int)t_flags.arg < 0)
+			ft_putchar_fd('-', 1);
 		while (zeroes-- > 0)
 			ft_putchar_fd('0', 1);
-		ft_putstr_fd(ft_itoa((int)t_flags.arg), 1);
+		if (!(t_flags.period && t_flags.precision == 0))
+			ft_putstr_fd(ft_itoa(num), 1);
 		while (spaces-- > 0)
 			ft_putchar_fd(' ', 1);
 	}	
@@ -178,7 +216,7 @@ void ft_check_type(char **format){
 		ft_printf_string();
 	// else if (*format == 'p')
 	// 	ft_printf_pointer();
-	else if (*(*format) == 'd')
+	else if (*(*format) == 'd' || *(*format) == 'i')
 		ft_printf_int();
 	// else if (*format == 'i')
 	// 	ft_printf_octal(); //?
@@ -209,6 +247,7 @@ void updateStruct(){
 
 	while (*format)
 	{
+		
 		if (*format == '%')
 		{
 			updateStruct();
@@ -240,39 +279,47 @@ void updateStruct(){
 			// if(f[i] == '%')
 			// 	ft_putstr_fd(va_arg(args, char*),1);
 			// 	va_arg(args, int);
-		write(1, &(*format), 1);
-		format++;
+		else{
+			write(1, &(*format), 1);
+			//printf("\n%soof", format);
+			format++;
+		}	
+		
 	}
 	//printf("%s", format);
 	va_end(args);
 	return (0);
 }
 
+//"./sources"
 
 
 
-int		main()
-{
-	// char *c = "1111";
-	// printf("%d", ft_isdigit(*c));
-	//check_identifiers("%-0247.478h");
-	// printf("%-5.*s", 3, "asdsa");
-	ft_printf("%18.15sh", "Hello");
-	printf("\n%18.15s\n", "Hello");
+
+// int		main()
+//   {
+// // char *c = "1111";
+// // printf("%d", ft_isdigit(*c));
+// // check_identifiers("%-0247.478h");
+// // printf("%-5.*s", 3, "asdsa");
+// // printf("%18.15s\n", "Hello");
+//  	//ft_printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[','\\',']','^','_','`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','{','|','}','~','');
+// 	//ft_printf("%c%c%c", ' ','!','"');
+// 	printf("%5.4d\n", 0);
+// 	ft_printf("%5.4d", 0);
 
 
+// // ft_printf("%-10.*d %7cv hello\n", 8, 349857, 's');
+// // printf("%-10.*d %7cv hello\n", 8, 349857, 's');
 
-	// ft_printf("%-10.*d %7cv hello\n", 8, 349857, 's');
-	// printf("%-10.*d %7cv hello\n", 8, 349857, 's');
+// //printf("Hello %15chhh\n", 'b');
 
-	//printf("Hello %15chhh\n", 'b');
-
-	//printf("%-10d", 7);
+// //printf("%-10d", 7);
 	
 	
-	// printf("Zero: %d\n", t_flags.zero);
-	// printf("Max: %d\n", t_flags.maxWidth);
-	// printf("Min: %d\n", t_flags.minWidth);
-	// printf("Dot: %d\n", t_flags.period);
+// // printf("Zero: %d\n", t_flags.zero);
+// // printf("Max: %d\n", t_flags.maxWidth);
+// // printf("Min: %d\n", t_flags.minWidth);
+// // printf("Dot: %d\n", t_flags.period);
 
-}
+//   }
--- printf/tester.c
+++ printf/tester.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int		main()
-{
-	// char *c = "1111";
-	// printf("%d", ft_isdigit(*c));
-	//check_identifiers("%-0247.478h");
-	// printf("%-5.*s", 3, "asdsa");
-	//ft_printf("Hello %15chhh\n", 'b');
+// int		main()
+// {
+// 	// char *c = "1111";
+// 	// printf("%d", ft_isdigit(*c));
+// 	//check_identifiers("%-0247.478h");
+// 	// printf("%-5.*s", 3, "asdsa");
+// 	//ft_printf("Hello %15chhh\n", 'b');
 
-	printf("%7.10s", "hello");
+// 	printf("%7.10s", "hello");
 	
 	
-	// printf("Zero: %d\n", t_flags.zero);
-	// printf("Max: %d\n", t_flags.maxWidth);
-	// printf("Min: %d\n", t_flags.minWidth);
-	// printf("Dot: %d\n", t_flags.period);
+// 	// printf("Zero: %d\n", t_flags.zero);
+// 	// printf("Max: %d\n", t_flags.maxWidth);
+// 	// printf("Min: %d\n", t_flags.minWidth);
+// 	// printf("Dot: %d\n", t_flags.period);
 
-}
+// }
